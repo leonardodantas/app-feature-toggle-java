e0bee23f7b998356acf7050df28ce56b9b4b6773
leonardo dantas <leonardo1317@yahoo.com.br>
2024-03-10 08:00:00 -0300

    ajustes finais

--- src/main/java/com/app/feature/toggle/config/UnleashProperties.java
+++ src/main/java/com/app/feature/toggle/config/UnleashProperties.java
@@ -3,9 +3,11 @@ package com.app.feature.toggle.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
+@Configuration
 @ConfigurationProperties(prefix = "unleash")
 public class UnleashProperties {
 
